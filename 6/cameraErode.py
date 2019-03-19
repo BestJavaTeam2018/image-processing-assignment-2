0038c9e69ef73b48550ba27e19a51a1108356881
unknown <mohamed2m2018@gmail.com>
2019-03-19 14:00:08 +0200

    Adding my observation into the code

--- 6/cameraErode.py
+++ 6/cameraErode.py
@@ -11,6 +11,9 @@ img2=cv2.erode(img,kernel,iterations=1)
 
 #img3=cv2.dilate(img2,kernel,iterations=1)
 
+#my observation is unless I perform dilation on the image the salt noise
+# will not be removed completely, but it will just be reduced
+
 
 cv2.imshow('Your amazing filtered Image', img2)
 
