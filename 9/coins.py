c8cf5024b2d67934d6176cd0eb1e32a5cd5bf268
unknown <mohamed2m2018@gmail.com>
2019-03-19 17:27:11 +0200

    Solving problem 9 successfully

--- 9/coins.py
+++ 9/coins.py
@@ -8,19 +8,20 @@ gray = cv2.cvtColor(coins, cv2.COLOR_BGR2GRAY)
 # again, try different values here
 edged = cv2.Canny(gray, 600, 200)
 
-cv2.imshow('Edges', edged)
-
-contours, _ = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-print(len(contours))
+contours, _ = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
 amountOfMoneyCalculated  =0
 
 for element in contours:
     area=cv2.contourArea(element)
     if area>2000:
+        cv2.drawContours(coins, element, -1, (255,0,0), 3)
         amountOfMoneyCalculated+=50
-    else :   
+    else : 
+        cv2.drawContours(coins, element, -1, (0,0,255), 3)
         amountOfMoneyCalculated+=25
+
+cv2.imshow('coins',coins)        
 
 print(amountOfMoneyCalculated)
 
