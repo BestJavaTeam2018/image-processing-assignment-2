a60037a4ffdd3f51960f071f816c07734b29b8c4
unknown <mohamed2m2018@gmail.com>
2019-03-19 18:42:29 +0200

    Removing unnecessary comments

--- 2/dilation2.py
+++ 2/dilation2.py
@@ -1,16 +1,11 @@
 # pylint: disable=no-member
 
 import cv2
-import numpy as np
 
 img = cv2.imread('cameraman.png', 0)
 
 kernelSq=cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
 
-#kernel=np.ones((5,5), np.uint8)
-
-#opening=cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
-#closing=cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
 
 dilation=cv2.dilate(img,kernelSq,iterations=1)
 
