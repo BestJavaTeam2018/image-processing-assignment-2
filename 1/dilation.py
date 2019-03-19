9b276da75f49892a7660e3ae21650a004876d174
unknown <mohamed2m2018@gmail.com>
2019-03-19 20:37:15 +0200

    Removing some comments

--- 1/dilation.py
+++ 1/dilation.py
@@ -13,14 +13,9 @@ kernelcircle=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(11,11))
 dilation=cv2.dilate(img,kernelcircle,iterations=1)
 cv2.imshow('using circle structure', dilation)
 
-#convex boundaries will become rounded,
-#and concave boundaries will be preserved as they are.
-
-
 dilation=cv2.dilate(img,kernelSq,iterations=1)
 cv2.imshow('using square structure', dilation)
 
-#will dilate the bottom of a region more strongly than the top.
 
 
 
