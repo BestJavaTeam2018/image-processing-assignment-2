0683fdd50f90aae87ee9a06dcb38ed004c2f73cc
unknown <mohamed2m2018@gmail.com>
2019-03-19 15:28:06 +0200

    Counting circles in the separated circle image successfully

--- 7/separation.py
+++ 7/separation.py
@@ -1,11 +1,21 @@
 import cv2
 
+import numpy as np
+
 img = cv2.imread('circle_and_lines.png', 0)
 
 kernelDisk=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(11,11))
 
-opening=cv2.morphologyEx(img, cv2.MORPH_OPEN, kernelDisk)
+circles=cv2.morphologyEx(img, cv2.MORPH_OPEN, kernelDisk)
 
-cv2.imshow('Your amazing filtered Image', opening)
+
+cv2.imshow('Your circles separated', circles)
+
+(contours, _) = cv2.findContours(circles.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+#number of circles in the image
+
+print(len(contours))
+
 
 cv2.waitKey()
