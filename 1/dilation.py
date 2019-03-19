30537e43153bb1ca81331301e0ebee166a791f7f
unknown <mohamed2m2018@gmail.com>
2019-03-19 17:54:18 +0200

    Editing problem 1

--- 1/dilation.py
+++ 1/dilation.py
@@ -9,18 +9,16 @@ kernelSq=cv2.getStructuringElement(cv2.MORPH_RECT,(11,11))
 
 kernelcircle=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(11,11))
 
-#kernel=np.ones((11,11), np.uint8)
-
 
 dilation=cv2.dilate(img,kernelcircle,iterations=1)
-cv2.imshow('Your amazing filtered Image', dilation)
+cv2.imshow('using circle structure', dilation)
 
 #convex boundaries will become rounded,
 #and concave boundaries will be preserved as they are.
 
 
 dilation=cv2.dilate(img,kernelSq,iterations=1)
-cv2.imshow('Your amazing filtered Image2', dilation)
+cv2.imshow('using square structure', dilation)
 
 #will dilate the bottom of a region more strongly than the top.
 
