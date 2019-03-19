6239969d16e5016d3d2c9d5e52e9374c1a53f2fc
unknown <mohamed2m2018@gmail.com>
2019-03-19 18:40:57 +0200

    Editing problems 2,4

--- 2/dilation2.py
+++ 2/dilation2.py
@@ -4,11 +4,18 @@ import cv2
 import numpy as np
 
 img = cv2.imread('cameraman.png', 0)
-kernel=np.ones((5,5), np.uint8)
 
-opening=cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
-closing=cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
-cv2.imshow('Your amazing filtered Image', closing)
+kernelSq=cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
+
+#kernel=np.ones((5,5), np.uint8)
+
+#opening=cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+#closing=cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+
+dilation=cv2.dilate(img,kernelSq,iterations=1)
+
+
+cv2.imshow('Your amazing filtered Image', dilation)
 
 
 cv2.waitKey()
--- 4/erosion.py
+++ 4/erosion.py
@@ -3,15 +3,27 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('circle.png', 0)
-kernelSq=cv2.getStructuringElement(cv2.MORPH_RECT,(11,1))
+img = cv2.imread('Square-circle.png', 0)
 
-kernelCr=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(2,11))
 
-erosion=cv2.erode(img,kernelSq,iterations=1)
-erosion=cv2.erode(img,kernelCr,iterations=1)
 
-cv2.imshow('Your amazing filtered Image', erosion)
+kernelCr=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(11,11))
+
+kernelSq=cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
+
+
+circleErosion=cv2.erode(img,kernelCr,iterations=2)
+# the hole in the middle of the image increases in size as the border shrinks.
+# erosion using a disk shaped structuring element will tend to round concave boundaries
+#but will preserve the shape of convex boundaries.
+
+squareErosion=cv2.erode(circleErosion,kernelSq,iterations=1)
+
+
+cv2.imshow('squareErosion', squareErosion)
+
+#the image took a slightly squared shape 
+
 
 
 cv2.waitKey()
