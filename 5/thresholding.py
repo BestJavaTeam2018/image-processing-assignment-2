7c910de54bbc6a75eff0a4fee47167f370002092
unknown <mohamed2m2018@gmail.com>
2019-03-19 12:34:22 +0200

    Finishing problem 5 with 80% accuracy

--- 5/thresholding.py
+++ 5/thresholding.py
@@ -5,10 +5,10 @@ img = cv2.imread('circle.png', 0)
 
 _,threshold=cv2.threshold(img,90,255,cv2.THRESH_BINARY_INV)
 
-kernel=np.ones((5,5), np.uint8)
+kernelDisk=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(7,11))
 
-erosion=cv2.erode(threshold,kernel,iterations=1)
+erosion=cv2.erode(threshold,kernelDisk,iterations=2)
 
-cv2.imshow('Your amazing filtered Image', threshold)
+cv2.imshow('Your amazing filtered Image', erosion)
 
 cv2.waitKey()
